f802c290e6560402dff04be782b49e2363874194
irashrashmitha <98259908+irashrashmitha@users.noreply.github.com>
2022-07-28 20:19:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,15 +22,15 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94770746123'] //Owner number in aive msg
+global.premium = ['+94770746123'] //Owner Number info
+global.ownernomer = '+94770746123' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'Irash Rashmitha' //Owner Name
+global.botname = ' Rashiya Bot' //Bot Name
 
 global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.btnurl = 'https://github.com/irashrashmitha' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
